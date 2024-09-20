84c567b5ffbe0b9e7d16fa27c332c7fb83d332f7
Reaien <ivanduarteherrera@gmail.com>
2024-09-19 22:57:53 -0300

    register form working

--- src/components/CardBodyRegister.tsx
+++ src/components/CardBodyRegister.tsx
@@ -14,6 +14,36 @@ function CardBodyRegister({ onSubmit, register, errors }: Props) {
       <form onSubmit={onSubmit} className="space-y-4">
         <div>
           <label className="block text-sm font-medium text-gray-700">
+            Nombres
+          </label>
+          <input
+            type="text"
+            {...register("firstName", {
+              required: "Los nombres son obligatorios",
+            })}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+          />
+          {errors.firstName && (
+            <p className="text-red-600 text-sm">{errors.firstName.message}</p>
+          )}
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Apellidos
+          </label>
+          <input
+            type="text"
+            {...register("lastName", {
+              required: "Los apellidos son obligatorios",
+            })}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+          />
+          {errors.lastName && (
+            <p className="text-red-600 text-sm">{errors.lastName.message}</p>
+          )}
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
             email
           </label>
           <input
@@ -30,6 +60,7 @@ function CardBodyRegister({ onSubmit, register, errors }: Props) {
             Contraseña
           </label>
           <input
+            min={8}
             type="password"
             {...register("password", {
               required: "La contraseña es obligatoria",
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,8 +2,11 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import Card from "../components/Card";
 import CardBodyRegister from "../components/CardBodyRegister";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 export type FormValues = {
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
 };
@@ -16,8 +19,25 @@ function Register() {
     formState: { errors },
   } = useForm<FormValues>();
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
     console.log("Datos enviados:", data);
+    try {
+      //petición POST
+      const response = await axios.post(
+        "http://localhost:8080/api/users/register",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json", // Añadir el header Content-Type
+          },
+        }
+      );
+      // Manejar la respuesta exitosa
+      console.log("Usuario registrado con éxito:", response.data);
+    } catch (error) {
+      // Manejar errores
+      console.error("Error en el registro:", error);
+    }
   };
   return (
     <>
